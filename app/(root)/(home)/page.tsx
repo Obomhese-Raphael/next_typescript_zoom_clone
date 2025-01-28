9c40d54ff0ed4e55023ca185e16cf25eb59ced4a
Obomhese Raphael <obomheser@gmail.com>
2025-01-28 17:23:51 +0100

    Fixed the Params Issue

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting &nbsp;
+            Check upcoming meetings &nbsp;
             <Link href="/upcoming">Here</Link>
           </h2>
           <div className="flex flex-col gap-2">
--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -8,12 +8,18 @@ import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
+import { Metadata } from 'next';
 
-const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+interface PageProps {
+    params: { id: string };
+}
+
+const Meeting = ({ params: { id } }: PageProps) => {
     const [isSetupComplete, setIsSetupComplete] = useState(false);
     const { user, isLoaded } = useUser();
     const { call, isCallLoading } = useGetCallById(id);
-    if (!isLoaded || isCallLoading) return <Loader />
+
+    if (!isLoaded || isCallLoading) return <Loader />;
     return (
         <main className="h-screen w-full">
             <StreamCall call={call}>
@@ -26,7 +32,7 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
                 </StreamTheme>
             </StreamCall>
         </main>
-    )
-}
+    );
+};
 
-export default Meeting
+export default Meeting;
