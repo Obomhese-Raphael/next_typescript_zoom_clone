f20eff02d676ec0adcd0af424d5c1fdf854b4213
Obomhese Raphael <obomheser@gmail.com>
2025-03-13 12:09:09 +0100

    Refactor Meeting component and add Alert component for error handling

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,29 +1,40 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-"use client"
+'use client';
 
-import Loader from '@/components/Loader';
-import MeetingRoom from '@/components/MeetingRoom';
-import MeetingSetup from '@/components/MeetingSetup';
-import { useGetCallById } from '@/hooks/useGetCallById';
-import { useUser } from '@clerk/nextjs'
+import { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
-import { Metadata } from 'next';
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
 
-interface PageProps {
-    params: { id: string };
-}
+import { useGetCallById } from '@/hooks/useGetCallById';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+import Alert from '@/components/Alert';
 
-const Meeting = ({ params: { id } }: PageProps) => {
+const MeetingPage = () => {
+    const { id } = useParams();
+    const { isLoaded, user } = useUser();
+    const { call, isCallLoading } = useGetCallById(id!);
     const [isSetupComplete, setIsSetupComplete] = useState(false);
-    const { user, isLoaded } = useUser();
-    const { call, isCallLoading } = useGetCallById(id);
 
     if (!isLoaded || isCallLoading) return <Loader />;
+
+    if (!call) return (
+        <p className="text-center text-3xl font-bold text-white">
+            Call Not Found
+        </p>
+    );
+
+    // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+    const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+    if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+
     return (
         <main className="h-screen w-full">
             <StreamCall call={call}>
                 <StreamTheme>
+
                     {!isSetupComplete ? (
                         <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
                     ) : (
@@ -35,4 +46,4 @@ const Meeting = ({ params: { id } }: PageProps) => {
     );
 };
 
-export default Meeting;
+export default MeetingPage;
